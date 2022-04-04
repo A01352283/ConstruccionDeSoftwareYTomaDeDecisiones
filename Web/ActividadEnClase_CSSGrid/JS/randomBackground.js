6ee5215f56f2c322b90abd0a5ff2adff45c256de
A01352283 <a01352283@itesm.mx>
2022-04-04 12:03:29 -0500

    Ejercicios 4 y 6-9
    Termina el resto de la actividad y agrega una función que no sirve para colorear el subgrid

--- Web/ActividadEnClase_CSSGrid/JS/randomBackground.js
+++ Web/ActividadEnClase_CSSGrid/JS/randomBackground.js
@@ -1,17 +1,35 @@
 function main(){
     changeGridItemColors();
+    //changeSubContainerColors();
 }
 
+
 function changeGridItemColors(){
-    //Gets the main container 
+    //Gets the main container
     let mainContainer = document.getElementById('main-container');
     let children = mainContainer.children;
-    
+
     //Iterates through the children of the main container
     for (let i = 0; i < children.length; i++) {
+        //Main container
         let child = children[i];
         child.style.backgroundColor = randomColor();
-        child.textContent = i;
+        child.textContent = 'item ' + i;
+    }
+}
+
+function changeSubContainerColors(){
+    //If the html has a secondary container
+    if (document.getElementById('secondary-container') != null) {
+        let subContainer = document.getElementById('secondary-container');
+        let subChildren = subContainer.children;
+
+        //Secondary container
+        for (let j = 0; j < subChildren.length; j++) {
+            let subChild = subChildren[j];
+            subChild.style.backgroundColor = randomColor();
+            subChild.textContent = 'subitem ' + j;
+        }
     }
 }
 
